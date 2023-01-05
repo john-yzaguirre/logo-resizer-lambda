f8e9e02d6002ad36f055e9c1fde2fee6c5aa588b
john-yzaguirre <113550974+john-yzaguirre@users.noreply.github.com>
2023-01-05 15:37:41 -0500

    first commit

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,7 @@
 const { awscdk } = require('projen');
+
+const cdkVersion = '2.59.0';
+
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'john-yzaguirre',
   authorAddress: '113550974+john-yzaguirre@users.noreply.github.com',
@@ -11,5 +14,12 @@ const project = new awscdk.AwsCdkConstructLibrary({
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
+  deps: [
+    `@aws-cdk/aws-apigatewayv2-alpha@${cdkVersion}-alpha.0`,
+    `@aws-cdk/aws-apigatewayv2-integrations-alpha@${cdkVersion}-alpha.0`,
+    `aws-cdk-lib@${cdkVersion}`,
+  ],
+  stability: 'experimental',
 });
+project.jest.addTestMatch('**/?(*.)@(spec|test).[tj]s?(x)');
 project.synth();
